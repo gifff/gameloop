a83ff32e3df928b7affb72ab1af93c9f9f259a11
Gifary Dhimas <gifarydhimas@gmail.com>
2019-01-31 20:48:28 +0700

    Initial Commit

---
+++ gameloop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gameloop
+//
+//  Created by Gifary Dhimas Fadhillah on 31/01/19.
+//  Copyright © 2019 Gifary Dhimas Fadhillah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
